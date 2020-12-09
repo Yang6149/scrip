2ee0b26b433db8106679d549bb0eebf2a0e4f815
yangshen.614 <yangshen.614@bytedance.com>
2020-12-09 17:18:27 +0800

    1

--- readInfo.py
+++ readInfo.py
@@ -1,9 +1,24 @@
 cacheInfo = []
-def read():
+def read(src):
     with open('./ad.style.factory_cal.log', 'r') as f:
     for line in f.readlines():
         if "CacheHit" in line:
             cacheInfo.append(line)
+def b(begin,end){
+    out = []
+    n = begin
+    while(n<=end){
+        if n<10:
+            out.append('0'+str(n))
+        else:
+            out.append(str(n))
+    }
+}
+prefix = './ad.style.factory_cal.log.2020-12-09_'
+
+for i in b(16,17):
+    src = prefix+i
+    read(src)
 num = 0
 for i in cacheInfo:
     if num%100==0:
