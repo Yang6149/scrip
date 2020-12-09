c9aaa80bc143a4c7a55542dfd81f8da1be1acdf7
yangshen.614 <yangshen.614@bytedance.com>
2020-12-09 17:13:25 +0800

    1

--- readInfo.py
+++ readInfo.py
@@ -2,11 +2,10 @@ cacheInfo = []
 def read():
     with open('./ad.style.factory_cal.log', 'r') as f:
     for line in f.readlines():
-        if "map full ,Size" in line:
-            cacheInfo.append(line)
-        if "Cache change Size" in line:
-            cacheInfo.append(line)
-        if "error happen there" in line:
-            cacheInfo.append(line)
         if "CacheHit" in line:
             cacheInfo.append(line)
+num = 0
+for i in cacheInfo:
+    if num%100==0:
+        print(i)
+    num +=1
